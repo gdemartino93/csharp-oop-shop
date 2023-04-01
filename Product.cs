dd97a90882b3a31fded53a6984250c7e08cc9ec3
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-04-01 13:25:50 +0200

    create product class

---
+++ Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Product
+    {
+        public int CodiceProdotto { get; set; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public decimal Prezzo { get; set; }
+        public int Iva { get; set; }
+    }
+}
